d46a8eb5b3701fa6fc8db12da1339ddad1480331
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-03-10 18:07:22 +0530

    Add files via upload

---
+++ area of triangle.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+float bredth,height,area;
+printf("ener bredth of trangle");
+scanf("%f",&bredth);
+printf("ener height of trangle");
+scanf("%d",&height);
+area=0.5*bredth*height;
+
+printf("area of trangle:%f",area);
+return(0);
+	
+}
+
